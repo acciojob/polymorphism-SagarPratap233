6b3cec3786b662473d5c19bcc354b79940b796bf
SagarPratap233 <sagarpratapsp@gmail.com>
2023-09-03 14:26:47 +0530

    fourth attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,10 +6,13 @@ public class Main {
 
 
         public int product (int x, int y){
+            return x*y;
         }
         public int product (int x, int y, int z) {
+            return x*y*z;
         }
         public double product (double x, double y) {
+            return x*y;
         }
     }
 
