5184d4e9fdf217c21c47e21bea8ddb29b21c064f
SagarPratap233 <sagarpratapsp@gmail.com>
2023-09-03 14:17:43 +0530

    thirdt attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,23 +2,20 @@ package com.driver;
 
 public class Main {
 
-    public class Product{
+    public static class Product{
 
 
         public int product (int x, int y){
-            return x*y;
         }
         public int product (int x, int y, int z) {
-            return x*y*z;
         }
         public double product (double x, double y) {
-            return x*y;
         }
     }
 
     Product p = new Product();
-    int x = p.product(1,2);
-    int y = p.product(1,2,3);
-    double z = p.product(0.1, 0.2);
+    p.product(1,2);
+    p.product(1,2,3);
+    p.product(0.1, 0.2);
 
 }
